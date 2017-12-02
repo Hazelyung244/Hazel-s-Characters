8b72b9245e33c60fa0d9f5bb4ac61fca96243ba9
Yee Kay Hazel Yung <Hazelyung24@yahoo.com>
2017-12-02 12:55:43 +0000

    initial commit

---
+++ src/main/java/HazelCharactersMain.java
@@ -0,0 +1,9 @@
+@SpringBootApplication
+@RestController
+public class HazelCharactersMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelCharactersMain.class, args);
+    }
+
+}
